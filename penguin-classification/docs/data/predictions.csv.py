eae92c81ac5049ed14a86b76903927f43688cd68
dleeftink <dennisleeftink@gmail.com>
2024-02-17 18:32:16 +1000

    Update predictions.csv.py

--- penguin-classification/docs/data/predictions.csv.py
+++ penguin-classification/docs/data/predictions.csv.py
@@ -6,7 +6,8 @@ import sys
 df = pd.read_csv("docs/data/penguins.csv")
 
 # Select columns to train the model
-X = df.iloc[:, [2, 3, 4, 5]]
+X = df.iloc[:, [2]]
+# X = df.iloc[:, [2, 3, 4, 5]]
 Y = df.iloc[:, 0]
 
 # Create an instance of Logistic Regression Classifier and fit the data.
