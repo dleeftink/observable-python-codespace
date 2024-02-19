23a9e6576a3eebc20b5c4d1fdf3e30d4c768f978
dleeftink <dennisleeftink@gmail.com>
2024-02-19 20:44:09 +1000

    Create setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,45 @@
+echo 'Please wait for setup to finish ...'
+
+# get repo
+
+if [ $(find . -maxdepth  1 -type d ! -name . ! -name .devcontainer | wc -l) -eq 0 ]; 
+then 
+
+  mv .devcontainer .. 
+  git clone "$target_repo" . 
+  rm -rf .devcontainer 
+  git clean -f -f
+  mv ../.devcontainer . 
+
+else 
+
+  echo Contents available :
+
+fi
+
+# install python
+
+if [ "$(whoami)" = "root" ]; 
+then  
+  
+  micromamba create -p base python -c conda-forge -y
+  export PATH="/opt/conda/envs/base/bin:$PATH"
+  
+  bash -c \
+    'eval "$(micromamba shell hook -s bash )"' \ 
+    'micromamba activate base'
+
+  # exec bash 
+
+else 
+
+  micromamba install python -c conda-forge -y
+
+fi
+
+# project dependencies installed via package.json
+# and postinstall script
+
+npm install
+
+exec bash
