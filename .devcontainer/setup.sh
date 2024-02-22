18ecbc1d922f2fd3c30c0f9df7ead5aed5d48825
dleeftink <dennisleeftink@gmail.com>
2024-02-23 03:15:33 +1000

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
 echo 'Please wait for setup to finish ...'
-# eval "$(micromamba shell hook --shell bash )" 
+eval "$(micromamba shell hook --shell bash )" 
 
 # get repo
 
 if [ $(find . -maxdepth  1 -type d ! -name . ! -name .devcontainer | wc -l) -eq 0 ]; 
 then 
 
+# move files selectively 
+
   mv .devcontainer .. 
   git clone "$target_repo" . 
+  
+  mv -f ../.devcontainer/devcontainer.json .devcontainer
+  rm -rf ../.devcontainer 
+
+# remove cloned and reinstate original .devcontainer folder
 # rm -rf .devcontainer 
 # git clean -f -f
 # mv ../.devcontainer . 
-  mv -f ../.devcontainer/devcontainer.json .devcontainer
-  rm -rf ../.devcontainer 
 
 else 
 
@@ -22,11 +27,11 @@ else
 
 fi
 
-# micromamba activate 
-# micromamba install -y -n base -f .devcontainer/env.yaml
-# micromamba clean --all --yes
+  micromamba activate 
+  micromamba install -y -n base -f .devcontainer/env.yaml
+  micromamba clean --all --yes
 
 # project dependencies installed via package.json
 # and postinstall script
 
-# npm install
+  npm install
